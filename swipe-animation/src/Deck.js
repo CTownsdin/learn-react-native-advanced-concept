1e6a875e6cc68a31f663957893682f582bcc37e9
bkdev98 <bkdev98@gmail.com>
2017-05-04 18:36:08 +0700

    detecting minium swipe distance

--- swipe-animation/src/Deck.js
+++ swipe-animation/src/Deck.js
@@ -7,6 +7,7 @@ import {
 } from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = SCREEN_WIDTH * 0.25;
 
 class Deck extends Component {
   constructor(props) {
@@ -19,7 +20,15 @@ class Deck extends Component {
       onPanResponderMove: (event, gesture) => {
         this.state.position.setValue({ x: gesture.dx, y: gesture.dy });
       },
-      onPanResponderRelease: () => { this.resetPosition(); },
+      onPanResponderRelease: (event, gesture) => {
+        if (gesture.dx > SWIPE_THRESHOLD) {
+          //  swipe right
+        } else if (gesture.dx < -SWIPE_THRESHOLD) {
+          //  swipe left
+        } else {
+          this.resetPosition();
+        }
+      },
     });
 
     this.state = { panResponder, position };
