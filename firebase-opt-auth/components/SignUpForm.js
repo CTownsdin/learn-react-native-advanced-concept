7a97dfda20b9fdf64d21c2990992db051f00efd4
bkdev98 <bkdev98@gmail.com>
2017-05-07 17:58:37 +0700

    invoking cloud functions

--- firebase-opt-auth/components/SignUpForm.js
+++ firebase-opt-auth/components/SignUpForm.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { FormLabel, FormInput, Button } from 'react-native-elements';
+import axios from 'axios';
+
+const ROOT_URL = 'https://us-central1-one-time-project-7f632.cloudfunctions.net';
 
 class SignUpForm extends Component {
   state = { phone: '' };
 
   handleSubmit = () => {
-
+    axios.post(`${ROOT_URL}/createUser`, {
+      phone: this.state.phone
+    })
+      .then(() => {
+        axios.post(`${ROOT_URL}/requestOTP`, {
+          phone: this.state.phone
+        })
+      })
   }
 
   render() {
