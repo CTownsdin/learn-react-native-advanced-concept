6006521a6aa73175baa4dcabe9aead54dccd26dc
bkdev98 <bkdev98@gmail.com>
2017-05-07 11:53:30 +0700

    separate OTP project

--- one-time-password/functions/twilio.js
+++
@@ -1,7 +0,0 @@
-const twilio = require('twilio');
-const config = require('./twilio_config.json');
-
-const accountSid = config.accountSid;
-const authToken = config.authToken;
-
-module.exports = new twilio.Twilio(accountSid, authToken);
