94540a33d5fca660a748bda5461dc54fbd01ef94
bkdev98 <bkdev98@gmail.com>
2017-05-08 22:43:00 +0700

    fix rawtext error

--- firebase-opt-auth/components/SignUpForm.js
+++ firebase-opt-auth/components/SignUpForm.js
@@ -6,10 +6,10 @@ import axios from 'axios';
 const ROOT_URL = 'https://us-central1-one-time-project-7f632.cloudfunctions.net';
 
 class SignUpForm extends Component {
-  state = { phone: '', error: '' };
+  state = { phone: '', error: null };
 
   handleSubmit = async () => {
-    this.setState({ error: '' });
+    this.setState({ error: null });
     try {
       await axios.post(`${ROOT_URL}/createUser`, { phone: this.state.phone })
       await axios.post(`${ROOT_URL}/requestOTP`, { phone: this.state.phone })
