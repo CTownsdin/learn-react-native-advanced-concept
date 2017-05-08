f882e4a9ec5ae035556be639a400b33d4c9e3e41
bkdev98 <bkdev98@gmail.com>
2017-05-08 23:28:48 +0700

    add loading to signInForm

--- firebase-opt-auth/components/SignInForm.js
+++ firebase-opt-auth/components/SignInForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 import { Text, FormLabel, FormInput, Button, FormValidationMessage } from 'react-native-elements';
 import axios from 'axios';
 import firebase from 'firebase';
@@ -7,16 +7,17 @@ import firebase from 'firebase';
 const ROOT_URL = 'https://us-central1-one-time-project-7f632.cloudfunctions.net';
 
 class SignInForm extends Component {
-  state = { phone: '', code: '', error: null };
+  state = { phone: '', code: '', error: null, loading: false };
 
   handleSubmit = async () => {
-    this.setState({ error: null });
+    this.setState({ error: null, loading: true });
     try {
       let { data } = await axios.post(`${ROOT_URL}/verifyOTP`, { phone: this.state.phone, code: this.state.code })
 
       firebase.auth().signInWithCustomToken(data.token);
+      this.setState({ loading: false });
     } catch (err) {
-      this.setState({ error: 'Something went wrong' });
+      this.setState({ error: 'Something went wrong', loading: false });
     }
   }
 
@@ -41,13 +42,16 @@ class SignInForm extends Component {
           />
         </View>
 
-        {this.state.error && <FormValidationMessage>{this.state.error}</FormValidationMessage>}
+        {this.state.error && <FormValidationMessage containerStyle={{ marginBottom: 10 }}>{this.state.error}</FormValidationMessage>}
 
-        <Button
-          title="Submit"
-          buttonStyle={{ backgroundColor: '#8E24AA' }}
-          onPress={this.handleSubmit}
-        />
+        {this.state.loading ? <ActivityIndicator color="#8E24AA" size="large" /> :
+          <Button
+            title="Submit"
+            buttonStyle={{ backgroundColor: '#8E24AA' }}
+            onPress={this.handleSubmit}
+          />
+        }
+
       </View>
     );
   }
