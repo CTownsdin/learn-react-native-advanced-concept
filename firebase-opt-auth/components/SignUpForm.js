203425edb3e6227582dc17988d55971bbcac2d60
bkdev98 <bkdev98@gmail.com>
2017-05-08 23:32:58 +0700

    add loading to signUpForm

--- firebase-opt-auth/components/SignUpForm.js
+++ firebase-opt-auth/components/SignUpForm.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 import { Text, FormLabel, FormInput, Button, FormValidationMessage } from 'react-native-elements';
 import axios from 'axios';
 
 const ROOT_URL = 'https://us-central1-one-time-project-7f632.cloudfunctions.net';
 
 class SignUpForm extends Component {
-  state = { phone: '', error: null };
+  state = { phone: '', error: null, loading: false };
 
   handleSubmit = async () => {
-    this.setState({ error: null });
+    this.setState({ error: null, loading: true });
     try {
       await axios.post(`${ROOT_URL}/createUser`, { phone: this.state.phone })
       await axios.post(`${ROOT_URL}/requestOTP`, { phone: this.state.phone })
+
+      this.setState({ loading: false });
     } catch (err) {
-      this.setState({ error: 'Something went wrong' });
+      this.setState({ error: 'Something went wrong', loading: false });
     }
   }
 
@@ -30,11 +32,14 @@ class SignUpForm extends Component {
           />
           {this.state.error && <FormValidationMessage>{this.state.error}</FormValidationMessage>}
         </View>
-        <Button
-          title="Submit"
-          buttonStyle={{ backgroundColor: '#8E24AA' }}
-          onPress={this.handleSubmit}
-        />
+
+        {this.state.loading ? <ActivityIndicator size="large" color="#8E24AA" /> :
+          <Button
+            title="Submit"
+            buttonStyle={{ backgroundColor: '#8E24AA' }}
+            onPress={this.handleSubmit}
+          />
+        }
       </View>
     );
   }
