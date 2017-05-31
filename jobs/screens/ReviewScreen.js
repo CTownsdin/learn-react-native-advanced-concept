b896d98012afc007b2e1b464900104e4208ea801
bkdev98 <bkdev98@gmail.com>
2017-05-31 16:43:13 +0700

    styling job list

--- jobs/screens/ReviewScreen.js
+++ jobs/screens/ReviewScreen.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text, Platform, ScrollView } from 'react-native';
+import { View, Text, Platform, ScrollView, Linking } from 'react-native';
 import { Button, Card } from 'react-native-elements';
 import { connect } from 'react-redux';
+import { MapView } from 'expo';
 
 @connect(state => {
   return { likedJobs: state.likedJobs };
@@ -24,13 +25,40 @@ class ReviewScreen extends Component {
 
   renderLikedJobs() {
     return this.props.likedJobs.map(job => {
+      const {
+        company,
+        formattedRelativeTime,
+        url,
+        latitude,
+        longitude,
+        jobtitle,
+        jobkey,
+      } = job;
+      const initialRegion = {
+        latitude,
+        longitude,
+        latitudeDelta: 0.045,
+        longitudeDelta: 0.02,
+      };
+
       return (
-        <Card>
+        <Card title={jobtitle} key={jobkey}>
           <View style={{ height: 200 }}>
+            <MapView
+              style={{ flex: 1 }}
+              cacheEnabled={Platform.OS === 'android'}
+              scrollEnabled={false}
+              initialRegion={initialRegion}
+            />
             <View style={styles.detailWrapper}>
-              <Text style={styles.italics}>{job.company}</Text>
-              <Text style={styles.italics}>{job.formattedRelativeTime}</Text>
+              <Text style={styles.italics}>{company}</Text>
+              <Text style={styles.italics}>{formattedRelativeTime}</Text>
             </View>
+            <Button
+              title="Apply Now!"
+              backgroundColor="#03A9F4"
+              onPress={() => Linking.openURL(url)}
+            />
           </View>
         </Card>
       );
@@ -48,6 +76,7 @@ class ReviewScreen extends Component {
 
 const styles = {
   detailWrapper: {
+    marginTop: 8,
     marginBottom: 10,
     flexDirection: 'row',
     justifyContent: 'space-around',
